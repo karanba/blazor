de5679dfcd6e0e1de1aa8465bbca452f9ad1eb20
Altay <Karakus>
2020-08-28 19:39:06 +0300

    added new web api for customer

---
+++ Gear/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Microsoft.IdentityModel.Tokens.JWT;
+
+namespace Gear
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var token = new JWTSecurityToken(
+                issuer: "http://myIssuer",
+                audience: "http://myResource",
+                claims: GetClaims(),
+                signingCredentials: GetKey(),
+                validForm: DateTime.UtcNow,
+                validTo: DateTime.UtcNow.AddHours(1)
+            );
+
+            var tokenString =
+                new JWTSecurityTokenHandler().WriteToken(token);
+
+            Console.WriteLine(tokenString);
+        }
+    }
+}
